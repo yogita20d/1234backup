0ecc42fef797023e770d064e4fb5f1ca9aacf030
H2_9_19_User31 <H2_9_19_User31@172.31.33.162>
2020-02-14 07:23:21 +0000

    yogitha code for test me app

---
+++ demo123/src/demo123/Launch1.java
@@ -0,0 +1,15 @@
+package demo123;
+
+public class Launch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		String S="LKM";
+	System.out.println("My first selenium program");
+	WebDriver driver;
+	
+
+	}
+
+}
